20347cdeb68cc71641c828bc795ed8a493bb2d7a
Shelly Pritchard <shelly.pritchard@wunderkind.co>
2025-07-03 16:08:59 +0530

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "Wunderkind",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "Wunderkind",
+            targets: ["Wunderkind"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Wunderkind",
+            url: "https://storage.googleapis.com/wunderkind-ios-sdk/3.0.0.zip",
+            checksum: "9a58d041745235908e0e41274683e8fc46745f5daa1a62a46e55d53b4e15ab8d"
+        )
+    ]
+)
+
